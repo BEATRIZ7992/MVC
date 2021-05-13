e1ebe5f8a20ec2dc5533a38b68fc2052a4d87237
bea7992 <beacampos127@gmail.com>
2021-05-13 18:09:34 +0200

    no message

---
+++ MvcMovie/Controllers/accesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class accesController : Controller
+    {
+        // GET: acces
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Entrar(string usuario, string password) 
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+
+            {
+                return Content("Ocurrio un error" + ex.Message);
+            }
+        }
+
+ 
+    }
+}
